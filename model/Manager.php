a890752aec9a8451a646aad6b26257458e953b05
THB <16556866+moezovic@users.noreply.github.com>
2018-09-29 10:34:19 +0200

    Ajouter des Managers aux tables blogposts et comments

---
+++ model/Manager.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace OpenClassrooms\ProBlog\Model;
+
+class Manager{
+
+	const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
+	const DB_USER = 'root';
+	const DB_PSW = 'root';
+
+	protected $connection;
+
+	protected function checkConnection(){
+		if ($this->connection === null) 
+		{
+			return $this->getConnection();
+		}
+
+		return $this->connection;
+	}
+
+	protected function getConnection(){
+		$this->connection = new \PDO(self::DB_HOST, self::DB_USER, self::DB_PSW);
+		$this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+		return $this->$connection;
+
+	protected function sql($sql,$parameters = null)
+	{
+		if ($parameters)
+		{
+			$result = $this->checkConnection()->prepare($sql);
+			$result->execute($parameters);
+			return $result;
+
+		}
+		else
+		{
+			$result = $this->checkConnection()->query($sql);
+			return $result;
+		}
+	}
+
+	}
+}
